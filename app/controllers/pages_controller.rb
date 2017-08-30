75e71bc45efd59ca6dd6bc8e9872936f51b266b7
tenzin2017 <tenzinleo@gmail.com>
2017-08-30 15:49:32 -0400

    different values are assigned for instance variable @header in diff instance method

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,15 @@
 class PagesController < ApplicationController
 
   def welcome
+    @header = "This is the welcome page"
   end
 
   def about
+      @header = "welcome page"
   end
 
   def contest
+      @header = "page"
   end
 
 end
