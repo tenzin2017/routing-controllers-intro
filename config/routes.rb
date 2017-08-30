85477f347f5efe0894d1ca2a140b7f357da00fb3
tenzin2017 <tenzinleo@gmail.com>
2017-08-30 17:32:07 -0400

    route helpers created for kiiten,kittens,secrets

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/' => 'pages#welcome'
+  root 'pages#welcome'
   get '/welcome' => 'pages#welcome'
   get '/about' => 'pages#about'
   get '/contest' => 'pages#contest'
-  get '/secrets/:magic_word' => 'pages#secrets'
-  get '/kitten/:size' => 'pages#kitten'
-  get '/kittens/:size' => 'pages#kittens'
 
+  get '/kitten/:size' => 'pages#kitten', as: 'kitten'
+  get '/kittens/:size' => 'pages#kittens', as: 'kittens'
+  get '/secrets/:magic_word' => 'pages#secrets', as: 'secrets'
 end
